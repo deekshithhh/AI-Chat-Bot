ce1662b8de042f8f483c28e0f85a8f9851d9fd74
Deekshith <deekshith3538@gmail.com>
2024-08-19 16:52:05 +0530

    SideBar and Navbar

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,12 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-import Landing from './Pages/Landingpage';
+import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import Sidebar from './Components/SideBar/Sidebar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Grid } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#D7C7F4',
+      light: '#fff',
+      dark: '#AF9FCD',
+      bglight: '#fafafa',
+      bgtheme: '#FAF7FF',
+      bg: '#AF9FCD'
+  },
+  text: {
+      primary: '#000000',
+      secondary: 'rgba(0,0,0,0.5)',
+  },
+    
+  }
+});
 
 function App() {
+
+  // const [mode, setMode] = useState(localStorage.getItem('theme') || 'light')
+  const [chat, setChat] = useState([])
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  //create theme
+  // const theme = React.useMemo(() => createTheme(getThemePallete(mode)), [mode]);
+
+  //save theme mode in localstorage
+  // useEffect(() => {
+
+  //   localStorage.setItem('theme', mode)
+
+  // }, [mode])
+
   return (
-    <div>
-      <Landing/>
-    </div>
+    
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Grid container sx={{ background: 'linear-gradient(rgba(215, 199, 244, 0.2), rgba(151, 133, 186, 0.2))' }} >
+          <Grid
+            item xs={12}
+            md={2.5}
+            sx={{
+              bgcolor: 'primary.light',
+              '@media (max-width:800px)': {
+                width: '70%',
+                transform: menuOpen ? 'translateX(0)' : 'translateX(-100%)',
+                transition: 'transform 400ms ease',
+              },
+            }}
+            position={{ xs: 'fixed', md: 'relative' }}
+            height={'100vh'}
+            zIndex={{ xs: 9999, md: 1 }}
+            boxShadow={{ xs: menuOpen ? 10 : 0, md: 0 }}
+          >
+            <Sidebar setChat={setChat} closeMenu={() => setMenuOpen(false)} />
+          </Grid>
+          <Grid item xs={12} md={9.5}>
+            <Outlet context={{ chat: chat, setChat: setChat, handleMobileMenu: setMenuOpen }} />
+          </Grid>
+        </Grid>
+
+      </ThemeProvider>
+   
   );
 }
 
--- chatbot/src/index.js
+++ chatbot/src/index.js
@@ -2,37 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '##9785BA',
-      secondary:'#AF9FCD'
-    },
-    secondary: {
-      main: '##D7C7F4',
-   
-    },
-  },
-  typography: {
-    fontFamily: 'Ubuntu,Sans-Serif',
-  },
-});
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import History from './Pages/History'
+import Home from './Pages/Home'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />
+      },
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
